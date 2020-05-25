d2d8dfc182158f31b064b38cf03838b2bf111735
Euzebio <euzebiosantanawork@gmail.com>
2020-05-25 11:48:32 -0300

    version1.0

---
+++ class4_SEO.js
@@ -0,0 +1,40 @@
+// Class 4 -SEO - Search Engine Optimization
+ 
+/* In short, and the art of influencing
+somehow the racking algorithms of the mechanisms
+search results, taking our results to better positions.
+ 
+UNDERSTAND THE DIFFERENCE BETWEEN
+Organic Traffic X Paid Traffic
+ 
+Traffic Paid, stay on top of the page
+Organic Traffic. is in the middle of the page
+
+HOW MUCH DOES AN AD COST?
+in the mechanics with which Google built AdSense, the terms
+search engines are leioloded. Its cost is expressed in CPC
+(Cost Per Click)
+ 
+HOW GOOGLE RULES
+Larry Page and Sergei Bryan created the algorithm in
+decades, and that algorithm has evolved a lot since then.
+It takes many factors into account, and very few teams then
+have permission to touch it.
+There are more than 4k of variavis kept under lock and key. Neither
+google's own engineers know them all
+ 
+SEO SURVIVAL KIT
+a few google ads + SEO tools
++ empirical test + good sense
+ 
+PAGERANK - FACTORS: INFRA
+- Loading speeds
+- https: // (encrypted pages)
+ 
+PAGERANK - FACTORS: SYNTAX
+- title, h1, h2, h3
+- meta tags
+- hml followed w3c standards
+https://w3.org/standarsds/
+- responsive
+http: /ami.responsivedesign.is
