230506e84b32e62e97928684773b6900fdeeef51
Euzebio <euzebiosantanawork@gmail.com>
2020-05-25 11:47:20 -0300

    version1.0

---
+++ class3_CSS.js
@@ -0,0 +1,271 @@
+// Class 3 - Framework CSS
+/*CSS - Structure
+"NASA landed robots on Mars, and here we are still
+struggling to align our divs to the center! "Unknown DEV
+
+CSS has a historical library of commands. Some
+companies and devs create some standards to facilitate the
+development of certain types of pages and components.
+Structures are the requirements 
+
+ 
+http://geekflare.com/best-css-frameworks
+
+
+BUT WHAT IS A FRAMEWORK?
+ 
+  It is an abstraction that joins common codigs between several projects of
+software providing generic functionality.
+  And a base structure, a development platform, as a
+kind of framework. it contains tools, guides, themes
+and components that streamline the development process
+of solutions.
+The big challenge is the business solution, not the technical solution.
+ 
+FRAMEWOEKS MVC: Laravel, Spring, Django, Rails
+ // What is MVC?
+    -Model view Controller
+    -Software Architectural Desing Paattern
+    -One of the most frequently used patterns
+    -Separates application functionality
+    -Promotes organized programming
+    Some Web frameworks That Use MVC Concepts
+    -Ruby on Rails (Ruby)
+    -Sinatra(Ruby)
+    -Zend(PHP)
+    -Express(JS)
+    -Backbone(JS)
+    -Angular (JS)
+    -Django (Python)
+    -Flask (Python)
+ 
+    MODEL
+    -Gata related logic
+    -INteraction with database (SELECT, INSERT< UPDATE, DELETE)
+    -Communicates with controller
+    -Can sometimes update the view (Depends on framework)
+ 
+    VIEW
+    -What the end user sees (UI)
+    -Usually consists of HTML/CSS
+    -Communicates with the controller
+    -Can be passed dynamic values from the controller
+    Template Engines
+ 
+    CONTROLLER
+    -Receives input (from view, url)
+    -Processes requests (GET,POST,PUT, DELETE)
+    -Gets data from the model
+    -Passes data to the view
+     
+   
+ 
+ 
+FRAMEWORKS JS: React Native, Angular, Vue
+   https://www.youtube.com/watch?v=_P2fGeg9I5o   
+   Choosing your JavaScript Framework
+   What Do We Look At?
+   Scale
+   Features
+   Supoort
+   Longevity
+   Performance
+   Popularity
+   Active Community
+   Documentation
+ 
+   Surveys and Polls
+   Surveys Have Tremendous Value
+ 
+   Dont Make this the only Data you process
+ 
+   Features: Componets, Performance, Statemanagement, Lazy Loading
+   Common Language/Transpilers
+ 
+   Componet Communication
+   -In React is Props
+   -In Angular is Inputs
+   - In Vue is Props
+ 
+   Lazy Loading
+   -In React is componets
+   -In Angular is Sets of Components
+   -In Vue is Components
+ 
+   Common Language / Transpilers
+   -In React is JavaScript
+   -In Angular is TypeScript // TypeScript is gorwing in each
+   -in Vue is Babel
+  
+   Standout Features
+   -In React is Components
+   -In Angular is full Framework
+   -In Vue is Simplicity
+   Features: You can build sucessful apps with each
+ 
+   Life is an education. It is learned ostly
+ 
+ 
+ 
+FRAMEWORKS MOBILE: React Native, Ionic, Xamarin, Flutter
+ 
+   https://www.youtube.com/watch?v=4m7msadL5iA
+ 
+   Choosing the Best Mobile App Framework
+   Considerations: Time to market, development costs, hiring,
+   maintenace costs, app performance, feature availaibillity,
+   3* aprty risks
+   FIRST PARTY NATIVE
+   apple + xcode
+   google + andoridstudio
+   -framework owner: apple/google
+   -Ui/Ux: native
+   -decelopment speed: Slow
+   -maintenance cost: High
+   -performance: very high
+   -proframming language: swift/ java
+   -maturity: very mature
+   -feature acess: complete
+   -open source: no
+  
+   PROGRESSIVE WEB APPS/ feature access:
+   GOOD: bluetooth, local notifications, touch gestures,
+   geolocation,camera, device motion, offline store, file acess.
+   NOTSGOOD: push notifications, battery status, vibration,
+   geofencing, augmented reality, outhers
+  
+   PROGRESSIVE WEB APPS/ Installing a PWA on mobile
+   -does not require app store
+   -installs locally to device
+   -runs as an installed app
+ 
+   CROSS-PLATFORM NATIVE APPS/popular frameworks
+   -xamarin
+   -react native
+   -flutter
+   -uno
+   -kotlin native
+ 
+   CROSS-PLATFORM NATIVE
+   -framework owner: microsoft
+   -UI/UX: Native
+   -Development Speed: Fast
+   -maintenance Cost: Low
+   -performance: high
+   -programming language: .NET, C#,F#
+   -Maturity: Mature
+   -Feature Access: Complete
+   -Open Source: Yes
+ 
+   CROSS-PLATFORM NATIVE
+   -framework owner: facebook
+   -UI/UX: Native
+   -Development Speed: Fast
+   -maintenance Cost: Low
+   -performance: high
+   -programming language: JavaScript
+   -Maturity: Mature
+   -Feature Access: Complete
+   -Open Source: Yes
+   *facebook + webstorm + visualstudiocode + atom + nuclide
+   frameworks: blazor native script, angularJS, react, ionic
+  
+   CROSS-PLATFORM NATIVE
+   -framework owner: Google
+   -UI/UX: Bespoke
+   -Development Speed: Fast
+   -maintenance Cost: Medium
+   -performance: High
+   -programming language: Dart
+   -Maturity: Young
+   -Feature Access: Complete*
+   -Open Source: Yes
+   *updates relased via NPM
+ 
+   Scenario
+   Simple APP
+   -Existing website created in AngularJS
+   -Enginnering team of web devs
+   -Doens`t require many features
+ 
+   Augmented Reality App
+   -Venture Capitol Funding
+   -Weary of thirt party frameworks
+   -risk adverse
+ 
+   choose the best framework for your team
+APPLIED TO CSS
+The idea of the CSS framework is that you follow a standard
+development without creating "new classes" in CSS but
+just specialize some styles for things that are
+application (with colors and sizes).
+EXAMPLE: And if our menu code was already, ready
+it was only necessary to import the CSS and create an item
+<ul> with the class "navbar"?
+FRAMEWORK CSS - COST / BENEFIT
+PROS:
+-standards development
+-reusable knowledge in another project
+that uses the same framework
+-Apps and websites look like the framework (which can
+match user platform)
+- you don't need a CSS expert to make some screens
+(creates cost benefit that can be worthwhile in the world of
+development)
+- Earn updates on a recurring basis
+
+AGAINST:
+- Difficult to change the framework after
+the project is already a few years old (tying the project up)
+-Not every CSS framework is ripe for new
+front-end technologies (React, Angular, etc.)
+-All sites and applications look the same, and
+it pisses you off over time
+ 
+Most popular CSS FRAMEWORKS: Bootstrap, Foudation,
+Pure CSS, Semantic UI, Materialize, Bulma, Uikit, Skeleton
+BOOTSTRAP
+Created by Twitter Inc., bootstrap, and undoubtedly the leader of
+all front-end frameworks available on the market.
+ 
+PROS:
+- Rapid prototyping. Speaker system leaves the factory
+working if the right classes are applied, leaving
+the responsive page.
+- Largest ecosystem of all frameworks
+- Maintained by Twitter
+-Has basically all the components that you will use
+in a common product
+ 
+CONS:
+- Ux monotony - because it is the most popular framework, all
+the pages look more or less the same face.
+- Little Flexibility - Getting out of line is very difficult
+- Heavy (~ 270KB out-of-the-box)
+MATERIALIZE
+Materializes and a modern framework based on Material
+Design by Google. The framework is free, but the team
+responsible for the project also offers more complete themes
+that can be purchased for a very reasonable price.
+ 
+PROS:
+-Follow the Bootstrap 12-speaker system
+-allows a clean starting look, taking advantage of
+Google components
+- Device-agnostic (runs well on mobile, desktop, etc.)
+- It basically has all the components that you will use
+in a common product
+ 
+CONS:
+- Little flexibility - Getting out of line and very difficult - Framework
+"opinionated"
+- Heavy (270KB start)
+- Does not support Flexbox (a more robust alternative for
+  the 12-speaker system)
+
+FONDATION
+  Foudation is a framework that calls itself "the framework
+  most advanced front-end response in the world. "Usually
+  adopted as a car full of developers instead the
+  Bootstrp, and less opinionated and maintained by ZURP.
+* /
